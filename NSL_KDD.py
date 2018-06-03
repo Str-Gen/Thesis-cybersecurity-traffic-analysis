ac42092eb90942eef4d8a420992c9d1c3d8d15af
Str-Gen <laurens.dhooge@telenet.be>
2018-06-03 14:23:14 +0200

    consistency changes

--- NSL_KDD.py
+++ NSL_KDD.py
@@ -90,14 +90,14 @@ if F == 16:
     nominal_cols = relevant16[nominal_indexes].tolist()
     numeric_cols = relevant16[numeric_indexes].tolist()
     dataframe = dataframe[relevant16]
-    #if A != 'DTree':
-        # one hot encoding for categorical features
+    # if A != 'DTree':
+    # one hot encoding for categorical features
     for cat in nominal_cols:
         one_hot = pd.get_dummies(dataframe[cat])
         dataframe = dataframe.drop(cat, axis=1)
         dataframe = dataframe.join(one_hot)
 if F == 41:
-    #if A != 'DTree':
+    # if A != 'DTree':
         # one hot encoding for categorical features
     for cat in nominal_cols:
         one_hot = pd.get_dummies(dataframe[cat])
@@ -182,7 +182,7 @@ X = np.delete(array, label_loc, 1)
 def kNN_with_k_search(data, cross=0, k_start=1, k_end=101, k_step=2, distance_power=2):
     gt0 = time()
     for k in range(k_start, k_end, k_step):
-        crossed[k] = []
+        crossed['knn:k'+repr(k)] = []
     for k in range(k_start, k_end, k_step):
         sys.stdout.write('Round %d, k = %d \r' % (cross, k))
         sys.stdout.flush()
@@ -190,108 +190,140 @@ def kNN_with_k_search(data, cross=0, k_start=1, k_end=101, k_step=2, distance_po
             n_neighbors=k, p=distance_power, n_jobs=-1)
         classifier.fit(data['X_train'], data['Y_train'])
         result = classifier.score(data['X_test'], data['Y_test'])
-        crossed[k].append([result, time()-gt0])
+        crossed['knn:k'+repr(k)].append([result, time()-gt0])
     return crossed
 
 
 def kNN_with_k_fixed(data, k, distance_power):
     gt0 = time()
-    crossed[k] = []
+    crossed['knn:k'+repr(k)] = []
     classifier = KNeighborsClassifier(n_neighbors=k, p=distance_power)
     classifier.fit(data['X_train'], data['Y_train'])
     result = classifier.score(data['X_test'], data['Y_test'])
-    crossed[k].append([result, time()-gt0])
+    crossed['knn:k'+repr(k)].append([result, time()-gt0])
     return crossed
 
-def linSVC_with_tol_iter_search(data, cross=0, tol_start=0, tol_end=-9, iter_start=0, iter_end=7):    
-    for tol_exp in range(tol_start,tol_end-1,-1):
-        for iter_exp in range(iter_start, iter_end+1):
-            gt0 = time()
-            crossed['tol1e'+repr(tol_exp)+':iter1e'+repr(iter_exp)] = []
-            classifier = LinearSVC(penalty='l2',tol=10**tol_exp, max_iter=10**iter_exp, dual=False)
-            classifier.fit(data['X_train'],data['Y_train'])
-            result = classifier.score(data['X_test'],data['Y_test'])
-            crossed['tol1e'+repr(tol_exp)+':iter1e'+repr(iter_exp)].append([result,time()-gt0])
-    return crossed
 
-def linSVC_with_tol_iter_fixed(data,tolerance,iterations):
-    gt0 = time()
-    classifier = LinearSVC(penalty='l2', tol=tolerance,max_iter=iterations, dual=False)
-    classifier.fit(X_train,Y_train)
-    result = classifier.score(X_test,Y_test)
-    crossed['linSVC'] = []
-    crossed['linSVC'].append([result,time()-gt0])
-    return crossed
-    
-def binLR_with_tol_iter_search(data,cross=0,tol_start=0,tol_end=-9,iter_start=0,iter_end=7):
-    for tol_exp in range(tol_start,tol_end-1,-1):
+def linSVC_with_tol_iter_search(data, cross=0, tol_start=0, tol_end=-9, iter_start=0, iter_end=7):
+    for tol_exp in range(tol_start, tol_end-1, -1):
         for iter_exp in range(iter_start, iter_end+1):
-            sys.stdout.write('Testing tolerance = %d with iterations = %d \r' % (tol_exp, iter_exp))
+            sys.stdout.write('Round %d Testing tol = %f with iterations= %f \r' % (
+                cross, 10**tol_exp, 10**iter_exp))
             sys.stdout.flush()
             gt0 = time()
-            crossed['tol1e'+repr(tol_exp)+':iter1e'+repr(iter_exp)] = []
-            classifier = LogisticRegression(penalty='l2',tol=10**tol_exp, max_iter=10**iter_exp, dual=False,n_jobs=-1)
-            classifier.fit(data['X_train'],data['Y_train'])
-            result = classifier.score(data['X_test'],data['Y_test'])
-            crossed['tol1e'+repr(tol_exp)+':iter1e'+repr(iter_exp)].append([result,time()-gt0])
+            crossed['linSVC:tol1e'+repr(tol_exp)+':iter1e'+repr(iter_exp)] = []
+            classifier = LinearSVC(penalty='l2', tol=10 **
+                                   tol_exp, max_iter=10**iter_exp, dual=False)
+            classifier.fit(data['X_train'], data['Y_train'])
+            result = classifier.score(data['X_test'], data['Y_test'])
+            crossed['linSVC:tol1e'+repr(tol_exp)+':iter1e' +
+                    repr(iter_exp)].append([result, time()-gt0])
     return crossed
 
-def binLR_with_tol_iter_fixed(data,tolerance,iterations):
+
+def linSVC_with_tol_iter_fixed(data, tolerance, iterations):
     gt0 = time()
-    classifier = LogisticRegression(penalty='l2', tol=tolerance,max_iter=iterations, dual=False,n_jobs=-1)
-    classifier.fit(X_train,Y_train)
-    result = classifier.score(X_test,Y_test)
-    crossed['binLR'] = []
-    crossed['binLR'].append([result,time()-gt0])
+    classifier = LinearSVC(penalty='l2', tol=tolerance,
+                           max_iter=iterations, dual=False)
+    classifier.fit(X_train, Y_train)
+    result = classifier.score(X_test, Y_test)
+    crossed['linSVC:tol1e'+repr(tolerance)+':iter1e'+repr(iterations)] = []
+    crossed['linSVC:tol1e'+repr(tolerance)+':iter1e' +
+            repr(iterations)].append([metric, time()-gt0])
     return crossed
 
-def DTree_with_maxFeatures_maxDepth_search(data,cross=0,max_depth=5,max_features=251):    
-    possible_features = range(1,max_features,1)
-    possible_features.extend(['sqrt','log2',None])
-    for md in range(1,max_depth,4):
+
+def binLR_with_tol_iter_search(data, cross=0, tol_start=0, tol_end=-9, iter_start=0, iter_end=7):
+    for tol_exp in range(tol_start, tol_end-1, -1):
+        for iter_exp in range(iter_start, iter_end+1):
+            sys.stdout.write('Round %d Testing tolerance = %d with iterations = %d \r' % (
+                cross, tol_exp, iter_exp))
+            sys.stdout.flush()
+            gt0 = time()
+            crossed['binLR:tol1e'+repr(tol_exp)+':iter1e'+repr(iter_exp)] = []
+            classifier = LogisticRegression(
+                penalty='l2', tol=10**tol_exp, max_iter=10**iter_exp, dual=False, n_jobs=-1)
+            classifier.fit(data['X_train'], data['Y_train'])
+            result = classifier.score(data['X_test'], data['Y_test'])
+            crossed['binLR:tol1e'+repr(tol_exp)+':iter1e' +
+                    repr(iter_exp)].append([result, time()-gt0])
+    return crossed
+
+
+def binLR_with_tol_iter_fixed(data, tolerance, iterations):
+    gt0 = time()
+    classifier = LogisticRegression(
+        penalty='l2', tol=tolerance, max_iter=iterations, dual=False, n_jobs=-1)
+    classifier.fit(X_train, Y_train)
+    result = classifier.score(X_test, Y_test)
+    crossed['binLR:tol1e'+repr(tolerance)+':iter1e'+repr(iterations)] = []
+    crossed['binLR:tol1e'+repr(tolerance)+':iter1e' +
+            repr(iterations)].append([metric, time()-gt0])
+    return crossed
+
+
+def DTree_with_maxFeatures_maxDepth_search(data, cross=0, max_depth=5, max_features=251):
+    possible_features = range(1, max_features, 1)
+    possible_features.extend(['sqrt', 'log2', None])
+    for md in range(1, max_depth, 4):
         for mf in possible_features:
-            sys.stdout.write('Testing max_depth = %d with max_features = %s \r' % (md, mf))
+            sys.stdout.write(
+                'Round %d Testing max_depth = %d with max_features = %s \r' % (cross, md, mf))
             sys.stdout.flush()
             gt0 = time()
-            crossed['maxFeatures'+repr(mf)+':maxDepth'+repr(md)] = []
-            classifier = DecisionTreeClassifier(criterion='gini',splitter='best',max_depth=md,max_features=mf)
-            classifier.fit(data['X_train'],data['Y_train'])
-            result = classifier.score(data['X_test'],data['Y_test'])
-            crossed['maxFeatures'+repr(mf)+':maxDepth'+repr(md)].append([result,time()-gt0])
+            crossed['DTree:maxFeatures'+repr(mf)+':maxDepth'+repr(md)] = []
+            classifier = DecisionTreeClassifier(
+                criterion='gini', splitter='best', max_depth=md, max_features=mf)
+            classifier.fit(data['X_train'], data['Y_train'])
+            result = classifier.score(data['X_test'], data['Y_test'])
+            crossed['DTree:maxFeatures' +
+                    repr(mf)+':maxDepth'+repr(md)].append([result, time()-gt0])
     return crossed
 
-def DTree_with_maxFeatures_maxDepth_fixed(data,max_depth,max_features):
+
+def DTree_with_maxFeatures_maxDepth_fixed(data, max_depth, max_features):
     gt0 = time()
-    classifier = DecisionTreeClassifier(criterion='gini',splitter='best',max_depth=max_depth,max_features=max_features)
-    classifier.fit(X_train,Y_train)
-    result = classifier.score(X_test,Y_test)
+    classifier = DecisionTreeClassifier(
+        criterion='gini', splitter='best', max_depth=max_depth, max_features=max_features)
+    classifier.fit(X_train, Y_train)
+    result = classifier.score(X_test, Y_test)
     crossed['DTree:depth'+repr(max_depth)+':features'+repr(max_features)] = []
-    crossed['DTree:depth'+repr(max_depth)+':features'+repr(max_features)].append([result,time()-gt0])
+    crossed['DTree:depth'+repr(max_depth)+':features' +
+            repr(max_features)].append([result, time()-gt0])
     return crossed
 
-def RForest_with_maxFeatures_maxDepth_search(data,cross=0,max_depth=5,max_features=251):
-    possible_features = range(1,max_features,1)
-    possible_features.extend(['sqrt','log2',None])
-    for md in range(1,max_depth,4):
+
+def RForest_with_maxFeatures_maxDepth_search(data, cross=0, max_depth=5, max_features=251):
+    possible_features = range(1, max_features, 1)
+    possible_features.extend(['sqrt', 'log2', None])
+    for md in range(1, max_depth, 4):
         for mf in possible_features:
-            sys.stdout.write('Testing max_depth = %d with max_features = %s \r' % (md, mf))
+            sys.stdout.write(
+                'Round %d Testing max_depth = %d with max_features = %s \r' % (cross, md, mf))
             sys.stdout.flush()
             gt0 = time()
-            crossed['maxFeatures'+repr(mf)+':maxDepth'+repr(md)] = []
-            classifier = RandomForestClassifier(criterion='gini',max_depth=md,max_features=mf,n_jobs=-1)
-            classifier.fit(data['X_train'],data['Y_train'])
-            result = classifier.score(data['X_test'],data['Y_test'])
-            crossed['maxFeatures'+repr(mf)+':maxDepth'+repr(md)].append([result,time()-gt0])
+            crossed['RForest:maxFeatures'+repr(mf)+':maxDepth'+repr(md)] = []
+            classifier = RandomForestClassifier(
+                criterion='gini', max_depth=md, max_features=mf, n_jobs=-1)
+            classifier.fit(data['X_train'], data['Y_train'])
+            result = classifier.score(data['X_test'], data['Y_test'])
+            crossed['RForest:maxFeatures' +
+                    repr(mf)+':maxDepth'+repr(md)].append([result, time()-gt0])
     return crossed
 
-def RForest_with_maxFeatures_maxDepth_fixed(data,max_depth,max_features):
+
+def RForest_with_maxFeatures_maxDepth_fixed(data, max_depth, max_features):
     gt0 = time()
-    classifier = RandomForestClassifier(criterion='gini',max_depth=max_depth,max_features=max_features,n_jobs=-1)
-    classifier.fit(X_train,Y_train)
-    result = classifier.score(X_test,Y_test)
-    crossed['RForest:depth'+repr(max_depth)+':features'+repr(max_features)] = []
-    crossed['RForest:depth'+repr(max_depth)+':features'+repr(max_features)].append([result,time()-gt0])
+    classifier = RandomForestClassifier(
+        criterion='gini', max_depth=max_depth, max_features=max_features, n_jobs=-1)
+    classifier.fit(X_train, Y_train)
+    result = classifier.score(X_test, Y_test)
+    crossed['RForest:depth'+repr(max_depth) +
+            ':features'+repr(max_features)] = []
+    crossed['RForest:depth'+repr(max_depth)+':features' +
+            repr(max_features)].append([result, time()-gt0])
     return crossed
+
 
 crossed = {}
 for cross in range(0, 3):
@@ -304,17 +336,22 @@ for cross in range(0, 3):
         'Y_train': Y_train,
         'X_test': X_test,
         'Y_test': Y_test
-    }    
+    }
     if A == 'kNN':
-        crossed = kNN_with_k_search(data, cross=cross, k_start=1, k_end=5, k_step=2, distance_power=1)
+        crossed = kNN_with_k_search(
+            data, cross=cross, k_start=1, k_end=5, k_step=2, distance_power=1)
     elif A == 'linSVC':
-        crossed = linSVC_with_tol_iter_search(data,cross=cross, tol_start=0, tol_end=-9, iter_start=0, iter_end=7)
+        crossed = linSVC_with_tol_iter_search(
+            data, cross=cross, tol_start=0, tol_end=-9, iter_start=0, iter_end=7)
     elif A == 'binLR':
-        crossed = binLR_with_tol_iter_search(data,cross=cross,tol_start=0, tol_end=-9, iter_start=0, iter_end=7)
+        crossed = binLR_with_tol_iter_search(
+            data, cross=cross, tol_start=0, tol_end=-9, iter_start=0, iter_end=7)
     elif A == 'DTree':
-        crossed = DTree_with_maxFeatures_maxDepth_search(data,cross=cross,max_depth=751,max_features=F)
+        crossed = DTree_with_maxFeatures_maxDepth_search(
+            data, cross=cross, max_depth=751, max_features=F)
     elif A == 'RForest':
-        crossed = RForest_with_maxFeatures_maxDepth_search(data,cross=cross,max_depth=751,max_features=F)
+        crossed = RForest_with_maxFeatures_maxDepth_search(
+            data, cross=cross, max_depth=751, max_features=F)
 print
 
 for k in crossed:
@@ -323,8 +360,8 @@ for k in crossed:
     crossed[k] = [np.mean(accs), np.std(accs), np.mean(times), np.std(times)]
 
 
-validated = sorted(crossed.iteritems(), key=lambda (k,v): v[0],reverse=True)
-for topn in range(0,len(crossed)) if len(crossed)<5 else range(0,5):
+validated = sorted(crossed.iteritems(), key=lambda (k, v): v[0], reverse=True)
+for topn in range(0, len(crossed)) if len(crossed) < 5 else range(0, 5):
     # print '#',topn,'avg acc: {} stdev acc: {} avg time: {} stddev time: {}'.format(*validated[topn])
     print validated[topn]
 
